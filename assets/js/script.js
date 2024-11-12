44a8ef07a25734348057524f642a1cdfb223e48d
carlos <carloslospinos@gmail.com>
2024-11-12 02:45:06 +0100

    Version 2.0.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,31 @@ function ocultar(){
     const caja = document.getElementById("cajaoculta");
     caja.classList.toggle("oculto");
 }
+window.addEventListener("DOMContentLoaded", () => { document.documentElement.classList.add("dark-mode");
+});
+
+const toggleButton = document.getElementById("toggle-dark-mode");
+
+toggleButton.addEventListener("click", () => {
+  document.documentElement.classList.toggle("dark-mode");
+  document.querySelectorAll('.medio_fondo').forEach(element => {
+    element.classList.toggle('dark-mode');});
+document.querySelectorAll('nav').forEach(element => {
+        element.classList.toggle('dark-mode');});
+document.querySelectorAll('.div-proyectos').forEach(element => {
+        element.classList.toggle('dark-mode');});
+document.querySelectorAll('.medio_f').forEach(element => {
+        element.classList.toggle('dark-mode');});
+document.querySelectorAll('.ring').forEach(element => {
+        element.classList.toggle('dark-mode');});
+document.querySelectorAll('.descarga').forEach(element => {
+       element.classList.toggle('dark-mode');});
+document.querySelectorAll('.email').forEach(element => {
+    element.classList.toggle('dark-mode');});
+document.querySelectorAll('#copiarmail').forEach(element => {
+    element.classList.toggle('dark-mode');});
+document.querySelectorAll('.boton_modo').forEach(element => {
+    element.classList.toggle('dark-mode');});
+
+});
+
